e9d426ce079dabf8b7f3291feff10604612d2198
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-04-24 13:26:28 +0300

    creates a class that defines test cases for class user

--- passlock.py
+++ passlock.py
@@ -98,13 +98,15 @@ class Credentials():
         return False
     @classmethod
     def display_credentials(cls):
-        """
+        '''
         Method that returns all items in the credentials list
 
-        """
+        '''
         return cls.credentials_list
     
     def generatePassword(stringLength=8):
-        """Generate a random password string of letters and digits and special characters"""
+        '''
+        Generate a random password string of letters and digits and special characters
+        '''
         password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "~!@#$%^&*"
         return ''.join(random.choice(password) for i in range(stringLength))
---
+++ passlock_test.py
@@ -0,0 +1,8 @@
+import unittest
+from passlock import User
+from passlock import Credentials
+
+class TestClass(unittest,unittest.TestCase):
+    '''
+    A Test class that defines test cases for the User class.
+    '''
